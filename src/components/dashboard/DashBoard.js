9ee94157154a7fe7322ff1d95c5b745c14b38e6a
macxu <adherentxu@gmail.com>
2017-10-14 00:57:29 +0800

    add numbercard and countUp

--- src/components/dashboard/DashBoard.js
+++ src/components/dashboard/DashBoard.js
@@ -3,23 +3,27 @@ import Main from '../Main';
 import { withRouter } from 'react-router';
 import { Paper, Card } from 'material-ui';
 import { Row, Col } from 'react-bootstrap';
-import Lin from "./Line";
+import NumberCard from './component/NumberCard';
 
 class DashBoard extends React.Component{
     render(){
+
+        const cardstyle = {
+            width:200,
+        };
+
         return(
             <Main selectedDrawer="dashboard">
                 <Paper className="admin-paper" zDepth={3}>
                     <Row className="show-grid">
-                        <Col xs={6} md={4}>
-                            <Card style="width:200px;">
-                                <Lin />
-                            </Card>
+                        <Col md={4}>
+                            <NumberCard />
                         </Col>
-                        <Col xs={12} md={8}>
-                            <Card style="width:200px;">
-                                <Lin />
-                            </Card>
+                        <Col md={4}>
+                            <NumberCard />
+                        </Col>
+                        <Col md={4}>
+                            <NumberCard />
                         </Col>
                     </Row>
                 </Paper>
---
+++ src/components/dashboard/component/NumberCard.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types'
+import { Card, FontIcon } from 'material-ui';
+import CountUp from 'react-countup'
+
+class NumberCard extends Component{
+    render(){
+
+        const numbercard = {
+            padding: 32,
+            marginBottom: 24,
+            marginTop: 10,
+        };
+        
+        const fontIcon = {
+            float: 'left',
+            fontSize: 50,
+        };
+
+        const content = {
+            width: '100%',
+            paddingLeft: 78,
+        };
+
+        const title = {
+            fontSize: 16,
+            marginBottom: 10,
+        };
+
+        const number = {
+            fontSize: 24,
+            height: 15,
+        };
+
+        return(
+            <Card style={numbercard}>
+                <FontIcon className="material-icons" style={fontIcon}>home</FontIcon>
+                <div style={content}>
+                    <p style={title}>Online Review</p>
+                    <p style={number}> 
+                        <CountUp
+                            start={0}
+                            end={2781}
+                            duration={4}
+                            useEasing
+                            useGrouping
+                            separator=","
+                        />
+                  </p>
+                </div>
+            </Card>
+        );
+    }
+}
+
+export default NumberCard;
