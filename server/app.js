a1e720c35c05f9edc80ac01b52a448401d1771bd
macxu <adherentxu@gmail.com>
2017-10-16 13:12:34 +0800

    add mongodb and mongoose

--- server/app.js
+++ server/app.js
@@ -5,7 +5,12 @@ import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import rewrite from 'express-urlrewrite';
+import mongoose from 'mongoose';
+import env from './env';
 
+
+
+let port = 3333;
 
 var app = express();
 
@@ -13,10 +18,20 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.use(bodyParser.json());
 
-app.get('*', (req, res) => res.sendFile( path.resolve( __dirname, '..', 'build/index.html')))
+app.get('*', (req, res) => res.sendFile( path.resolve( __dirname, '..', 'build/index.html')));
 
-app.listen(3333, function(){
-    console.log('server running at 3333');
+
+const db = mongoose.createConnection(env.MongoDbUrl);
+
+db.on('error', function(error){
+  console.log(error);
 });
+db.on('connected', function () {    
+  console.log('Mongoose connection open to ' + env.MongoDbUrl);  
+});    
+
+
+app.listen(port);
+console.log('Server is running on: https://localhost:%s',port);
 
 module.exports = app;
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Footer extends Component{
+    render(){
+        return(
+            <div className="footer">
+                <div style={{textAlign: "center"}}>
+                    <a href="javascript:void(0)">hello</a><br/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
