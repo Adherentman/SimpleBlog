9dcbb12825dedbd4e3acf9e6f22e5a7325f985bc
macxu <adherentxu@gmail.com>
2017-11-05 15:38:53 +0800

    Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { AppBar, IconButton, RaisedButton } from 'material-ui';
+import { Url } from '../env';
 
 
 class Home extends Component {
     render() {
+        const { history } = this.props;
         const title = 'blog';
+
         return (
-            <div>
+            <div style={{backgroundColor:'#fff'}}>
             <AppBar
             showMenuIconButton={false}
             title={title}
@@ -29,7 +32,24 @@ class Home extends Component {
                     <h3 style={{textAlign:'center'}}>Using React、Material-UI</h3>
                     <RaisedButton label="Let's Go"
                     style={{alignItems:'center',marginLeft:165,marginTop:25}}
+                    onClick={() => {history.push(Url('_dashboard'))}}
                     />
+                </div>
+            </div>
+        </div>
+        <div style={{overflow:'auto'}}>
+            <div style={{padding:'48px'}}>
+                <h2 style={{marginBottom:'0.35em', fontSize:'25px', fontFamily:'"Roboto", "Helvetica", "Arial", sans-serif', lineHeight:'25px'}}>Quick Links</h2>
+                <div style={{lineHeight:'1.5em', fontSize:'1.3em', fontWeight:'300'}}>
+                    <div style={{width:'100%', display:'flex', flexWrap:'wrap'}}>
+                        <div style={{maxWidth:'50%', flexBasis:'50%'}}>
+                        <ui style={{listStyle:'none'}}>
+                        <li style={{paddingTop:'4px', paddingBottom:'4px', display:'list-item'}}><a style={{color:'inherit', textDecoration:'none'}} href="https://github.com/Adherentman">GitHub</a></li>
+                        <li style={{paddingTop:'4px', paddingBottom:'4px', display:'list-item'}}><a style={{color:'inherit', textDecoration:'none'}} href="http://www.adherentman.cn/">Old Blog</a></li>
+                        <li style={{paddingTop:'4px', paddingBottom:'4px', display:'list-item'}}><a style={{color:'inherit', textDecoration:'none'}} href="https://www.zhihu.com/people/xu-zi-hao-60/activities">Zhi Hu</a></li>
+                        </ui>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
