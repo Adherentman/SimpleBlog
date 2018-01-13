f0099b85ebdd16215729d3e9defe73ff568118da
macxu <adherentxu@gmail.com>
2018-01-13 14:35:15 +0800

    png

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,7 +27,7 @@ class Home extends Component {
         <div style={{flex:'1 0 100%'}}>
             <div style={{flex:'0 0 auto', color:'#fff', display:'flex', minHeight:'100vh', alignItems:'center', justifyContent:'center', backgroundColor:'#2196f3'}}>
                 <div style={{paddingTop:128, paddingBottom:128}}>
-                    <img src="/xzhlogo.png" style={{width:'100%', margin:'20px 0', height:'40vw', maxHeight:'230px'}}/>
+                    <img src="./xzhlogo.png" style={{width:'100%', margin:'20px 0', height:'40vw', maxHeight:'230px'}}/>
                     <h2 style={{textAlign:'center'}}>Code Crafted With Heart By Xzh,</h2>
                     <h3 style={{textAlign:'center'}}>Using React、Material-UI</h3>
                     <RaisedButton label="Let's Go"
